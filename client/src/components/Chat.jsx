dd8cc577570cf5d32e4fd47cf03113c05f8e9492
HariKrishna-28 <harikrishnac2002@gmail.com>
2021-09-30 21:39:32 +0530

    ui improvements

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -82,7 +82,7 @@ const Chat = ({ socket, name, room }) => {
                                         <span className={bg}>{messageBody.message}</span>
                                     </div>
                                     <div className="gap-3 py-1 text-gray-700" style={{ fontSize: "12px" }}>
-                                        <p className="font-semibold">{name === messageBody.author ? "You" : messageBody.author} &ensp; {new Date(Date.now()).getHours() === messageBody ? "Now" : messageBody.time}</p>
+                                        <p className="font-semibold">{name === messageBody.author ? "You" : messageBody.author} &ensp; {new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes() === messageBody.time ? "Now" : messageBody.time}</p>
                                         {/* <p>{new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes() === messageBody.time ? "Now" : messageBody.time}</p> */}
                                     </div>
 
