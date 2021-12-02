9ecbdd89edb442af599f1787ccc52f5698df1bf4
HariKrishna-28 <harikrishnac2002@gmail.com>
2021-12-02 09:40:57 +0530

    added loading animations

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,21 @@ import LoginButton from "./components/LoginButton";
 import Profile from "./components/Profile";
 // import Logo from "./assets/GitHub-Mark.png"
 import { useAuth0 } from '@auth0/auth0-react';
-import ProfileLink from "./components/ProfileLink";
-
+import ProfileLink from "./components/ProfileLink"
+import ScaleLoader from "react-spinners/ScaleLoader"
 
 
 function App() {
   const { isAuthenticated } = useAuth0()
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div className="flex flex-col items-center justify-center h-screen">
+    <ScaleLoader
+
+      color="rgb(38, 39, 48)"
+    />
+  </div>
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <LoginButton />
